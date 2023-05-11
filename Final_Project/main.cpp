8f0ea5b5a6eaff909d77641e6c2c147b416a6c21
fuiiiii <fui20310@gmail.com>
2023-05-11 16:33:49 +0800

    week13

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -1,46 +1,68 @@
 #include <stdio.h> ///Input/Output
 #include <GL/glut.h>
+#include "glm.h"
+GLMmodel * head = NULL;
+GLMmodel * body = NULL;
+GLMmodel * right_arm = NULL;
+GLMmodel * left_arm = NULL;
+int show[4] ={1,0,0,0};
 float teapotX = 0, teapotY = 0;
 FILE * fout = NULL;
 FILE * fin = NULL;
+void keyboard(unsigned char key,int x, int y)
+{
+    if( key=='0' ) show[0] = ! show[0];
+    if( key=='1' ) show[1] = ! show[1];
+    if( key=='2' ) show[2] = ! show[2];
+    if( key=='3' ) show[3] = ! show[3];
+    glutPostRedisplay();
+}
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    if(head==NULL)
+    {
+        head = glmReadOBJ("model/head.obj");
+        body = glmReadOBJ("model/body.obj");
+        right_arm = glmReadOBJ("model/right_arm.obj");
+        left_arm = glmReadOBJ("model/left_arm.obj");
+    }
     glPushMatrix();
-        glTranslatef(teapotX, teapotY, 0);
-        glutSolidTeapot(0.3);
+        glScalef(0.3,0.3,0.3);
+        glPushMatrix();
+            glTranslatef(teapotX,teapotY,0);
+            if(show[0]) glmDraw(head, GLM_MATERIAL);
+        glPopMatrix();
+        if(show[1]) glmDraw(body, GLM_MATERIAL);
+        if(show[2]) glmDraw(right_arm, GLM_MATERIAL);
+        if(show[3]) glmDraw(left_arm, GLM_MATERIAL);
     glPopMatrix();
+
     glutSwapBuffers();
 }
-
+int oldX=0, oldY=0;
 void mouse(int button, int state, int x, int y)
 {
-    teapotX = (x-150)/150.0;
-    teapotY = (150-y)/150.0;
     if(state==GLUT_DOWN)
     {
-        if(fout==NULL) fout = fopen("file4.txt", "w");
-
-        fprintf(fout,"%f %f\n",&teapotX ,&teapotY);
+        oldX = x;
+        oldY = y;
     }
-    display();
 }
-void keyboard(unsigned char key,int x,int y)
+void motion(int x, int y)
 {
-    if(fin==NULL)
-    {
-        fclose(fout);
-        fin = fopen("file4.txt", "r");
-    }
-    fscanf(fin, "%f%f", &teapotX ,&teapotY);
-    display();
+    teapotX += (x-oldX)/150.0;
+    teapotY -= (y-oldY)/150.0;
+    oldX = x;
+    oldY = y;
+    glutPostRedisplay();
 }
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE|GLUT_DEPTH);
-    glutCreateWindow("week12");
-
+    glutCreateWindow("week13");
+    glutMotionFunc(motion);
     glutDisplayFunc(display);
     glutKeyboardFunc(keyboard);
     glutMouseFunc(mouse);
